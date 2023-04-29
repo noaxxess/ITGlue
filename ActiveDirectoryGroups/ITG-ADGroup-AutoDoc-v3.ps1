5a21b3c8a03e5008e10ae89ed304b39057ce04d3
noaxxess <113463394+noaxxess@users.noreply.github.com>
2023-04-29 11:27:42 -0400

    Created function to match Flex Asset Names w IDs Took API Call to Retrieve Flex Assets out of loop Commented Out Write-Hosts

--- ActiveDirectoryGroups/ITG-ADGroup-AutoDoc-v3.ps1
+++ ActiveDirectoryGroups/ITG-ADGroup-AutoDoc-v3.ps1
@@ -14,21 +14,19 @@
 .NOTES
     Script Originally Created by CyberDrain- Kelvin Tegelaar
     Modified By: Noaxxess
-    Added: Parameters, comments, formatting, logic to check if group member are users or computers, function to match configs and tag
+    Added: Parameters, comments, formatting, logic to check if group member are users or computers, function to match configs and tag. Also took API call to get all Assets out of the main Foreach loop and created function that matches flexible asset names and returns Flexible asset IDs
     Date: 04/25/2023
 
 #>
 
 
 param (
-
+    #IT Glue API Key, not required if already added via 'Add-ITGlueAPIKey' command.
     [Parameter(ValueFromPipeline = $true, HelpMessage = "API Key")]
     [String]$APIKey,
-    #IT GLue API Key
-    [Parameter(ValueFromPipeline = $true, HelpMessage = "Organization ID")]
+    #IT Glue Organization ID
+    [Parameter(ValueFromPipeline = $true, Mandatory = $true, HelpMessage = "Organization ID")]
     [String]$OrgID
-    #IT GLue Organization ID
-
 )
 
 #Function to return IT Glue Configuration IDs based on Names
@@ -39,6 +37,7 @@ function Get-ITGlueConfigurationID {
     )
 
     foreach ($config in $Configurations) {
+        #If your tenant uses FQDN instead of HostNames use the this line to split on period and get the hostname.
         $ConfigName = ($Config.attributes.Name).Split('.')[0]
         if ($ConfigName -eq $Name) {
             return $Config.Id
@@ -46,11 +45,25 @@ function Get-ITGlueConfigurationID {
     }
 }
 
-#Set IT Glue Info
+function Get-ITGlueFlexAssetID {
+    param (    
+        [string]$AssetName,
+        [array]$FlexAssets
+    )
+
+    foreach ($Asset in $FlexAssets) {
+        if ($AssetName -eq $Name) {
+            return $Asset.Id
+        }
+    }
+}
+
+#Set Variables
 $APIEndpoint = "https://api.itglue.com"
-$FlexAssetName = "ITGLue AutoDoc - Active Directory Groups v3"
+$FlexAssetName = "AutoDoc- Active Directory Groups v3"
 $Description = "Lists all groups and users in them."
-$ITGlueConfigs = (Get-ITGlueConfigurations -organization_id 5713007).data
+$ITGlueConfigs = (Get-ITGlueConfigurations -organization_id $OrgID).data
+
 
 #####################################################################
 
@@ -67,20 +80,104 @@ else {
 Add-ITGlueBaseURI -base_uri $APIEndpoint
 Add-ITGlueAPIKey $APIKey
 
+# Write-Host $ITGlueConfigs.id
+
+# Checking if the FlexibleAsset Type exists.
+$FilterID = (Get-ITGlueFlexibleAssetTypes -filter_name $FlexAssetName).data
+
+# If the Flexible Asset Type does not exist create a new one
+if (!$FilterID) {
+    $NewFlexAssetData = @{
+        type          = 'flexible-asset-types'
+        attributes    = @{
+            name        = $FlexAssetName
+            icon        = 'sitemap'
+            description = $description
+        }
+        relationships = @{
+            "flexible-asset-fields" = @{
+                data = @(
+                    @{
+                        type       = "flexible_asset_fields"
+                        attributes = @{
+                            order           = 1
+                            name            = "Group Name"
+                            kind            = "Text"
+                            required        = $true
+                            "show-in-list"  = $true
+                            "use-for-title" = $true
+                        }
+                    },
+                    @{
+                        type       = "flexible_asset_fields"
+                        attributes = @{
+                            order          = 2
+                            name           = "Members"
+                            kind           = "Textbox"
+                            required       = $false
+                            "show-in-list" = $true
+                        }
+                    },
+                    @{
+                        type       = "flexible_asset_fields"
+                        attributes = @{
+                            order          = 3
+                            name           = "GUID"
+                            kind           = "Text"
+                            required       = $false
+                            "show-in-list" = $false
+                        }
+                    },
+                    @{
+                        type       = "flexible_asset_fields"
+                        attributes = @{
+                            order          = 4
+                            name           = "Tagged Users"
+                            kind           = "Tag"
+                            "tag-type"     = "Contacts"
+                            required       = $false
+                            "show-in-list" = $false
+                        }
+                    },
+                    @{
+                        type       = "flexible_asset_fields"
+                        attributes = @{
+                            order          = 5
+                            name           = "Tagged Configurations"
+                            kind           = "Tag"
+                            "tag-type"     = "Configurations"
+                            required       = $false
+                            "show-in-list" = $false
+                        } 
+                    }
+                )
+            }
+        }
+    }
+    # Write-Host "Creating New Flexible Asset Type"
+    New-ITGlueFlexibleAssetTypes -Data $NewFlexAssetData
+    $FilterID = (Get-ITGlueFlexibleAssetTypes -filter_name $FlexAssetName).data
+}
+
+#Write-Host $filterID.id
+
+#Get Existing Assets from IT Glue
+$ITGlueFlexAssets = (Get-ITGlueFlexibleAssets -filter_flexible_asset_type_id $Filterid.id -filter_organization_id $orgID).data
 
 # Get all AD Groups
 $AllGroups = Get-AdGroup -filter *
-#Lopp Through Groups
+
+#Loop Through Groups and populate flex asset body
 foreach ($Group in $AllGroups) {
     
     #Set Arrays for Asset Tags
     $Contacts = @()
     $Configs = @()    
     
-    #Loop through the Groups to get Members
+    #Get Group Members
     $Members = Get-AdGroupMember $Group
     
-    #Save the members into a table
+    #Save the members into a table formatted with HTML
     $MembersTable = $Members | Select-Object Name, SamAccountName, distinguishedName | ConvertTo-Html -Fragment | Out-String 
     
     #Loop Through Members
@@ -107,110 +204,35 @@ foreach ($Group in $AllGroups) {
             }     
         }
     }
-
+    #Add data to flex asset body that will be uploaded to API
     $FlexAssetBody = @{
         type       = 'flexible-assets'
         attributes = @{
             name   = $FlexAssetName
             traits = @{
-                "group-name"   = $($Group.Name)
-                "members"      = $MembersTable
-                "guid"         = $($Group.ObjectGuid.Guid)
-                "tagged-users" = $Contacts
+                "group-name"            = $($Group.Name)
+                "members"               = $MembersTable
+                "guid"                  = $($Group.ObjectGuid.Guid)
+                "tagged-users"          = $Contacts
                 "tagged-configurations" = $Configs
             }
         }
     }
 
-    # Checking if the FlexibleAsset exists.
-    $FilterID = (Get-ITGlueFlexibleAssetTypes -filter_name $FlexAssetName).data
+    # Get Existing ITGlue Flex Asset Data (if any) and match existing AD group data
+    $ExistingFlexAsset = Get-ITGlueFlexAssetID -FlexAssets $ITGlueFlexAssets -AssetName $($group.name) 
 
-    # If the Flexible Asset Type does not exist create a new one
-    if (!$FilterID) {
-        $NewFlexAssetData = @{
-            type          = 'flexible-asset-types'
-            attributes    = @{
-                name        = $FlexAssetName
-                icon        = 'sitemap'
-                description = $description
-            }
-            relationships = @{
-                "flexible-asset-fields" = @{
-                    data = @(
-                        @{
-                            type       = "flexible_asset_fields"
-                            attributes = @{
-                                order           = 1
-                                name            = "Group Name"
-                                kind            = "Text"
-                                required        = $true
-                                "show-in-list"  = $true
-                                "use-for-title" = $true
-                            }
-                        },
-                        @{
-                            type       = "flexible_asset_fields"
-                            attributes = @{
-                                order          = 2
-                                name           = "Members"
-                                kind           = "Textbox"
-                                required       = $false
-                                "show-in-list" = $true
-                            }
-                        },
-                        @{
-                            type       = "flexible_asset_fields"
-                            attributes = @{
-                                order          = 3
-                                name           = "GUID"
-                                kind           = "Text"
-                                required       = $false
-                                "show-in-list" = $false
-                            }
-                        },
-                        @{
-                            type       = "flexible_asset_fields"
-                            attributes = @{
-                                order          = 4
-                                name           = "Tagged Users"
-                                kind           = "Tag"
-                                "tag-type"     = "Contacts"
-                                required       = $false
-                                "show-in-list" = $false
-                            },
-                            @{
-                                type       = "flexible_asset_fields"
-                                attributes = @{
-                                    order          = 5
-                                    name           = "Tagged Configurations"
-                                    kind           = "Tag"
-                                    "tag-type"     = "Configurations"
-                                    required       = $false
-                                    "show-in-list" = $false
-                                }
-                            }
-                        }
-                    )
-                }
-            }
-        }
-        New-ITGlueFlexibleAssetTypes -Data $NewFlexAssetData
-        $FilterID = (Get-ITGlueFlexibleAssetTypes -filter_name $FlexAssetName).data
-    }
-
-    # Get Existing ITGlue Flex Asset Data and match our data
-    $ExistingFlexAsset = (Get-ITGlueFlexibleAssets -filter_flexible_asset_type_id $Filterid.id -filter_organization_id $orgID).data | Where-Object { $_.attributes.traits.'group-name' -eq $($group.name) }
 
     # If the Asset does not exist, we edit the body to be in the form of a new asset
     if (!$ExistingFlexAsset) {
+        # Write-Host "Creating new flexible asset"
         $FlexAssetBody.attributes.add('organization-id', $orgID)
         $FlexAssetBody.attributes.add('flexible-asset-type-id', $FilterID.id)
-        Write-Host "Creating new flexible asset"
-        New-ITGlueFlexibleAssets -data $FlexAssetBody
+        New-ITGlueFlexibleAssets -data $FlexAssetBody 
     }
     #Otherwise Just Upload the data
     else {
-        Write-Host "Updating Flexible Asset"
+        # Write-Host "Updating flexible asset"
         $ExistingFlexAsset = $ExistingFlexAsset[-1]
         Set-ITGlueFlexibleAssets -id $ExistingFlexAsset.id -data $FlexAssetBody
     }
